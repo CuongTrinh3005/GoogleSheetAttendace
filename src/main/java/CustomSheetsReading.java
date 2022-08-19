4667e1cd0a50365873407c379551fcaa2f5a4e47
quoc-cuong <cuongtrinhq@gmail.com>
2022-08-19 11:09:36 +0700

    add official sheet into integration

--- src/main/java/CustomSheetsReading.java
+++ src/main/java/CustomSheetsReading.java
@@ -19,7 +19,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class CustomSheetsReading {
@@ -61,24 +60,24 @@ public class CustomSheetsReading {
     public static void main(String... args) throws IOException, GeneralSecurityException {
         // Build a new authorized API client service.
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        final String spreadsheetId = "1AXCuR1rsi77guOiqX_YyKV-wQv28o99tIrQkspNiU3I";
-        final String range = "Custom!A3:C";
+        final String spreadsheetId = "1WPHRacPbpy3GXvOxbzGLPtqgOt9bb_Y1LQRLvbeab54";
+        final String range = "Record!A2:C";
         Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
                 .setApplicationName(APPLICATION_NAME)
                 .build();
 
         // Append data to sheet
-        ValueRange appendBody = new ValueRange()
-                .setValues(Arrays.asList(
-                        Arrays.asList("Nguyễn Thị", "Linh", "Frontend Engineer")));
-        AppendValuesResponse appendResult = service.spreadsheets().values()
-                .append(spreadsheetId, "A5", appendBody)
-                .setValueInputOption("USER_ENTERED")
-                .setInsertDataOption("INSERT_ROWS")
-                .setIncludeValuesInResponse(true)
-                .execute();
-        if(appendResult != null)
-            System.out.println("\nAppending data successfully!\n");
+//        ValueRange appendBody = new ValueRange()
+//                .setValues(Arrays.asList(
+//                        Arrays.asList("Nguyễn Thị", "Linh", "Frontend Engineer")));
+//        AppendValuesResponse appendResult = service.spreadsheets().values()
+//                .append(spreadsheetId, "A5", appendBody)
+//                .setValueInputOption("USER_ENTERED")
+//                .setInsertDataOption("INSERT_ROWS")
+//                .setIncludeValuesInResponse(true)
+//                .execute();
+//        if(appendResult != null)
+//            System.out.println("\nAppending data successfully!\n");
 
         // Read sheet's data
         ValueRange response = service.spreadsheets().values()
